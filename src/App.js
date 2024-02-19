ac7d61accca6d6b6d2614e4936244d912a99e374
Akilanpalani <akilanpalani2001@gmail.com>
2024-02-19 12:54:46 +0530

    feat: :sparkles: Fixed responsive design and bug fixed in functionalities

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
@@ -12,12 +12,16 @@ import CategoryPage from './components/CategoryPage';
 import CartPage from './pages/CartPage';
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+  const handleSearchQueryChange = (query) => {
+    setSearchQuery(query);
+  };
   return (
     <Provider store={store}>
       <Router>
-        <Navbar />
+        <Navbar onSearchQueryChange={handleSearchQueryChange} />
         <Routes>
-          <Route path='/' element={<HomePage />} />
+          <Route path='/' element={<HomePage searchQuery={searchQuery} />} />
           <Route path='/:id' element={<ProductDetails />} />
           <Route path='/category/:name' element={<CategoryPage />} />
           <Route path='/cart' element={<CartPage />} />
--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -7,26 +7,32 @@ const CartPage = () => {
   const cartItems = useSelector((state) => state.cart.cart);
   const dispatch = useDispatch();
   return (
-    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-5'>
+    <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-5 mb-5'>
       {cartItems && cartItems.length === 0 ? (
         <div className='flex justify-center items-center h-screen'>
-          <p className='text-2xl text-center'>Cart is empty</p>
+          <p className='text-2xl font-semibold sm:font-bold mb-4 text-center'>
+            Cart is empty
+          </p>
         </div>
       ) : (
         <div className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-5'>
-          <h2 className='text-2xl text-center font-bold'>Your Cart</h2>
-          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:justify-center sm:items-center mt-7'>
-            {cartItems.map((product) => (
-              <div className='max-w-[300px] flex flex-col'>
-                <ProductCards key={product.id} product={product} />
-                <button
-                  onClick={() => dispatch(clearCart(product.id))}
-                  className='bg-green-500 text-center'
-                >
-                  Clear Item
-                </button>
-              </div>
-            ))}
+          <h2 className='text-2xl font-semibold sm:font-bold mb-4 text-center'>
+            Your Cart
+          </h2>
+          <div className='flex flex-col items-center mx-3'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 sm:justify-center sm:items-center mt-7'>
+              {cartItems.map((product) => (
+                <div className='max-w-[300px] flex flex-col'>
+                  <ProductCards key={product.id} product={product} />
+                  <button
+                    onClick={() => dispatch(clearCart(product.id))}
+                    className='bg-green-500 text-center'
+                  >
+                    Clear Item
+                  </button>
+                </div>
+              ))}
+            </div>
           </div>
           <div className='flex justify-center mt-10'>
             <button
